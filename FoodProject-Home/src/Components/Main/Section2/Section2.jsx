8d65eb97db3070572d1c3f62b17434df3b1fbee4
Ruchi Shrivastava <39464621+ruchi-shrivastava@users.noreply.github.com>
2024-09-28 01:54:03 +0530

    new commit

--- FoodProject-Home/src/Components/Main/Section2/Section2.jsx
+++ FoodProject-Home/src/Components/Main/Section2/Section2.jsx
@@ -1,20 +1,23 @@
 import sectionImg  from '../../../assets/sectionImg.avif'
-import style from '../../../Components/Main/Section2/section2.module.css'
+import style from '../../../Components/Main/Section2/Section2.module.css'
 
 const Section2 = () => {
     return(
-        <>
+        <div className={style.section2Container}>
         <div>
             <img className={style.section2Img} src={sectionImg} />
         </div>
-        <div>
-            <div>
-                <h1>hello</h1>
-                <p></p>
-                <button></button>
+       
+            {/* <div className={style.section2Text}> */}
+                <div className={style.rightText}>
+                <h2 className={style.h1Text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, debitis.</h2>
+                <p className={style.pText}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, molestiae! Quidem est esse numquam odio deleniti, beatae, magni dolores provident quaerat totam eos, aperiam architecto eius quis quibusdam fugiat dicta.</p>
+                <button className={style.btn}>Get in Touch</button>
+                </div>
+               
             </div>
-        </div>
-        </>
+       
+        // </div>
     )
 }
 export default Section2
